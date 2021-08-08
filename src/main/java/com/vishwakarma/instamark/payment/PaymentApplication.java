02b0a761caca3f3fadfaf6caf30363e4fc640e7c
akash1905 <vakash609@gmail.com>
2021-08-08 16:30:47 +0530

    MSc - IT Project

---
+++ src/main/java/com/vishwakarma/instamark/payment/PaymentApplication.java
@@ -0,0 +1,14 @@
+package com.vishwakarma.instamark;
+
+import package com.vishwakarma.instamark.payment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalExampleApplication.class, args);
+	}
+
+}
